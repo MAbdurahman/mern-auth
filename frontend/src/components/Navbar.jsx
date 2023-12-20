0fa61b62e58dc310ddf4cc29dd64aecacf172c2e
MAbdurahman <mdbdrrhm@gmail.com>
2023-12-20 04:38:50 -0600

    Add tailwind css to components/Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,24 @@
+import {Link} from "react-router-dom";
+
 export function Navbar() {
    return (
-      <><h2>Navbar</h2></>
+      <div className='bg-slate-200'>
+         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+            <Link to='/'>
+               <h1 className='font-bold'>Auth App</h1>
+            </Link>
+            <ul className='flex gap-4'>
+               <Link to='/'>
+                  <li>Home</li>
+               </Link>
+               <Link to='/about'>
+                  <li>About</li>
+               </Link>
+               <Link to='/profile'>
+                  <li>Profile</li>
+               </Link>
+            </ul>
+         </div>
+      </div>
    )
 }
