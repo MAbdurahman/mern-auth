4451f054b72e703eea98a31dffa5da740d9433d4
MAbdurahman <mdbdrrhm@gmail.com>
2023-12-21 04:00:40 -0600

    Add backend/routes/authRoute.js endpoint for localhost:5000/api/v1.0/auth/sign-in

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,12 +5,16 @@ import User from "../models/userModel.js";
 
 export const signUp = async (req, res, next) => {
    const { username, email, password } = req.body;
-   /*if (username) {
-      return next(errorHandler('409', 'Username already exists!'));
+   if (!username) {
+      /*return next(errorHandler('409', 'Username already exists!'));*/
+      return next(errorHandler(400, 'Please enter a username!'));
    }
-   if (email) {
-      return next(errorHandler('409', 'Email already exists!'));
-   }*/
+   if (!email) {
+      return next(errorHandler(400, 'Please enter an email!'));
+   }
+   if (!password) {
+      return next(errorHandler(400, 'Please enter a password!'));
+   }
    const hashedPassword = bcryptjs.hashSync(password, 10);
    const newUser = new User({ username, email, password: hashedPassword });
 
@@ -22,3 +26,43 @@ export const signUp = async (req, res, next) => {
    }
 
 };
+
+export const signIn = async (req, res, next) => {
+   const { email, password } = req.body;
+
+   try {
+      if (!email) {
+         return next(errorHandler(400, 'Please enter email!'));
+      }
+      if (!password) {
+         return next(errorHandler(400, 'Please enter password!'))
+      }
+
+      const validUser = await User.findOne({ email });
+      if (!validUser) {
+         return next(errorHandler(403, 'Invalid Email or Password!'));
+      }
+
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if (!validPassword) {
+         return next(errorHandler(401, 'Invalid Email or Password!'));
+      }
+
+      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+      const { password: hashedPassword, ...rest } = validUser._doc;
+
+      const milliseconds_minute = 60000;
+      const milliseconds_hour = milliseconds_minute * 60;
+      const milliseconds_day = milliseconds_hour * 24;
+      const milliseconds_week = milliseconds_day * 7;
+      const expiryDate = new Date(Date.now() + milliseconds_week)
+
+      res
+         .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+         .status(200)
+         .json(rest);
+
+   } catch (error) {
+      next(error);
+   }
+}
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signUp } from "../controllers/authController.js";
+import { signUp, signIn } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post('/sign-up', signUp);
+router.post('/sign-in', signIn);
 
 
 
