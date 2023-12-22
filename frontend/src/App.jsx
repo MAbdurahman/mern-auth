04162f5f30c6fc5015d16c125bddb23a1d42fc80
MAbdurahman <mdbdrrhm@gmail.com>
2023-12-22 07:01:56 -0600

    Update frontend/src/components/Navbar.jsx and make frontend/src/pages/ProfilePage.jsx a private route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {Navbar} from "./components/Navbar.jsx";
 import {HomePage} from "./pages/HomePage.jsx";
 import {AboutPage} from "./pages/AboutPage.jsx";
@@ -6,19 +6,22 @@ import {SignInPage} from "./pages/SignInPage.jsx";
 import {SignUpPage} from "./pages/SignUpPage.jsx";
 import {ProfilePage} from "./pages/ProfilePage.jsx";
 import {NotFoundPage} from "./pages/NotFoundPage.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 export default function App() {
 
    return (
       <BrowserRouter>
-         <Navbar />
+         <Navbar/>
          <Routes>
-            <Route path={'/'} element={<HomePage/>} />
-            <Route path={'/about'} element={<AboutPage/>} />
-            <Route path={'/sign-in'} element={<SignInPage/>} />
-            <Route path={'/sign-up'} element={<SignUpPage/>} />
-            <Route path={'/profile'} element={<ProfilePage/>} />
-            <Route path={'/*'} element={<NotFoundPage/>} />
+            <Route path={'/'} element={<HomePage/>}/>
+            <Route path={'/about'} element={<AboutPage/>}/>
+            <Route path={'/sign-in'} element={<SignInPage/>}/>
+            <Route path={'/sign-up'} element={<SignUpPage/>}/>
+            <Route element={<PrivateRoute/>}>
+               <Route path={'/profile'} element={<ProfilePage/>}/>
+            </Route>
+            <Route path={'/*'} element={<NotFoundPage/>}/>
          </Routes>
       </BrowserRouter>
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import {Link} from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 export function Navbar() {
+   const { currentUser } = useSelector((state) => state.user);
+
    return (
       <div className='bg-slate-200'>
          <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -15,7 +18,11 @@ export function Navbar() {
                   <li>About</li>
                </Link>
                <Link to='/profile'>
-                  <li>Profile</li>
+                  {currentUser ? (
+                     <img src={currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full object-cover' />
+                  ) : (
+                     <li>Sign In</li>
+                  )}
                </Link>
             </ul>
          </div>
