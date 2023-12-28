6906987f91035aa8fc14463919f86e1488895176
MAbdurahman <mdbdrrhm@gmail.com>
2023-12-28 14:37:34 -0600

    Attempt to correct error when signing out in production

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,10 @@ import colors from 'colors';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import connectDatabase from './config/configDatabase.js';
-import Template from "./template.js";
+import Template from './template.js';
 
-import userRoutes from "./routes/userRoute.js";
-import authRoutes from "./routes/authRoute.js";
+import userRoutes from './routes/userRoute.js';
+import authRoutes from './routes/authRoute.js';
 
 //**************** configuration setup ****************//
 dotenv.config({path: 'backend/config/config.env'});
@@ -19,19 +19,19 @@ colors.enable();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV;
-const API_URL = process.env.API_ENV || "/api/v1.0/";
+const API_URL = process.env.API_ENV || '/api/v1.0/';
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/dist')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 })
 //**************** connect to database ****************//
 connectDatabase();
 
 //**************** middleware****************//
 if (process.env.NODE_ENV === 'DEVELOPMENT') {
-    app.use(morgan('dev'));
+   app.use(morgan('dev'));
 }
 
 app.use(express.json());
@@ -39,28 +39,23 @@ app.use(cookieParser());
 
 //**************** app listening ****************//
 const server = app.listen(PORT, () => {
-    console.log(`The server is listening at - http://127.0.0.1:${PORT}${API_URL} in ${NODE_ENV} mode🔥`.yellow);
+   console.log(`The server is listening at - http://127.0.0.1:${PORT}${API_URL} in ${NODE_ENV} mode🔥`.yellow);
 });
 
 //**************** routes****************//
-
-/*app.get('/api/v1.0/', (req, res) => {
-    res.send(Template());
-});*/
-app.get('/api/v1.0/', (req, res) => {
-    res.json({
-        message: 'API for mern-auth-app is working.'
-    });
-});
-
+if (process.env.NODE_ENV === 'DEVELOPMENT') {
+   app.get('/api/v1.0/', (req, res) => {
+      res.send(Template());
+   });
+}
 app.use('/api/v1.0/user', userRoutes);
 app.use('/api/v1.0/auth', authRoutes);
 
 //**************** handle errors middleware ****************//
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success: false, statusCode, message,
-    });
+   const statusCode = err.statusCode || 500;
+   const message = err.message || 'Internal Server Error';
+   return res.status(statusCode).json({
+      success: false, statusCode, message,
+   });
 });
